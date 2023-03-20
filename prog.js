1d5c0e9f1a63c6b81a7b29c32fb09cb2c3842fc5
Júlia <jfranca.contato@gmail.com>
2023-03-20 08:40:05 -0300

    erro no botão-js. css finalizado

--- prog.js
+++ prog.js
@@ -1,14 +1,14 @@
-let valor_pais = []
+let valor_pais = ['Chile', 'Brasil', 'Argentina']
 let res = document.getElementById('res')
 
-function mostrar() {
-    let pais = document.getElementById('pais').value
-    valor_pais.push(pais)
-    res.innerHTML = valor_pais
+function mostrar(){
+    // let pais = document.getElementById('pais').value
+    // valor_pais.push(pais)
+    res.innerHTML = valor_pais    
 }
 
-function excluir() {
-    valor_pais.pop(pais)
+function excluir(){
+    // valor_pais.pop(pais)
     res.innerHTML = valor_pais
 
 }
