47f8b026dd220f2f4a7743a56643181dc8f0c8fb
Júlia <jfranca.contato@gmail.com>
2023-03-20 10:55:53 -0300

    finalizado com comandos novos do array

--- prog.js
+++ prog.js
@@ -1,14 +1,21 @@
-let valor_pais = ['Chile', 'Brasil', 'Argentina']
+let pais = []
 let res = document.getElementById('res')
+//let cidades = new Array('Tijucas', 'Itajaí')
 
-function mostrar(){
-    // let pais = document.getElementById('pais').value
-    // valor_pais.push(pais)
-    res.innerHTML = valor_pais    
+function incluir(){
+    let nome_pais = document.getElementById('nome_pais').value
+    pais.push(nome_pais)
+    res.innerHTML = pais
+
+    //let incluir = document.getElementById('incluir')
+    //incluir.addEventListener('click', ()=>{
+        //res.innerHTML += pais + "<br>"
+        //res.innerHTML += cidades
+    //})
 }
 
 function excluir(){
-    // valor_pais.pop(pais)
-    res.innerHTML = valor_pais
+    pais.pop(pais)
+    res.innerHTML = pais
 
 }
